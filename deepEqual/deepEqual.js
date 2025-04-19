99eca196fd9749797a78539bc50b2c359bc4afcf
Stephen-Zulauf <szulauf1990@gmail.com>
2025-04-19 16:03:25 -0700

    still need to add recursive check for objects

--- deepEqual/deepEqual.js
+++ deepEqual/deepEqual.js
@@ -1,16 +1,49 @@
-'use strict';
+"use strict";
 // Don't add or change anything above this comment.
 
-/* 
-* TODO: add your First Name Last Name.
-*/
+/*
+ * Stephen Zulauf
+ * zulaufs@osu.edu
+ */
 
 /*
-* Don't change the declaration of this function.
-*/
+ * Don't change the declaration of this function.
+ */
 function deepEqual(val1, val2) {
+  //null check
+  if (val1 === null & val2 === null){
+    return true
+  }
+  if(val1 === null && val2 != null){
+    return false
+  }
+  if(val1 != null && val2 === null){
+    return false
+  }
+  //check if one value is a primitive and one is object
+  if (typeof(val1) === "object" && typeof(val2) != "object"){
+    return false
+  }
+  if (typeof(val1) != "object" && typeof(val2) === "object"){
+    return false
+  }
+  //check for deep equlity if both are primitives
+  if (typeof(val1) != "object" && typeof(val2) != "object"){
+    if(val1 === val2){
+        return true
+    }else{
+        return false
+    }
+  }
 
-}
+  //check for deep equality if both are objects
+  if (typeof(val1) === "object" && typeof(val2) === "object"){
+    if(val1 === val2){
+        return true
+    }else{
+        return false
+    }
+  }
 
 // Don't add or change anything below this comment.
 module.exports = deepEqual;

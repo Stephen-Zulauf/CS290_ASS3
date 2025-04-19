18fa8477f35b0c97309531e6ca121a05f80e012b
Stephen-Zulauf <szulauf1990@gmail.com>
2025-04-19 15:40:24 -0700

    init

---
+++ deepEqual/deepEqual.js
@@ -0,0 +1,16 @@
+'use strict';
+// Don't add or change anything above this comment.
+
+/* 
+* TODO: add your First Name Last Name.
+*/
+
+/*
+* Don't change the declaration of this function.
+*/
+function deepEqual(val1, val2) {
+
+}
+
+// Don't add or change anything below this comment.
+module.exports = deepEqual;

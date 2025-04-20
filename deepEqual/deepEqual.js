d40056ee07b910c11c812b74001a196ff94ebcde
Stephen-Zulauf <szulauf1990@gmail.com>
2025-04-19 17:13:35 -0700

    problems with recursive object check

--- deepEqual/deepEqual.js
+++ deepEqual/deepEqual.js
@@ -11,39 +11,60 @@
  */
 function deepEqual(val1, val2) {
   //null check
-  if (val1 === null & val2 === null){
-    return true
+  if ((val1 === null) & (val2 === null)) {
+    return true;
   }
-  if(val1 === null && val2 != null){
-    return false
+  if (val1 === null && val2 != null) {
+    return false;
   }
-  if(val1 != null && val2 === null){
-    return false
+  if (val1 != null && val2 === null) {
+    return false;
   }
   //check if one value is a primitive and one is object
-  if (typeof(val1) === "object" && typeof(val2) != "object"){
-    return false
+  if (typeof val1 === "object" && typeof val2 != "object") {
+    return false;
   }
-  if (typeof(val1) != "object" && typeof(val2) === "object"){
-    return false
+  if (typeof val1 != "object" && typeof val2 === "object") {
+    return false;
   }
   //check for deep equlity if both are primitives
-  if (typeof(val1) != "object" && typeof(val2) != "object"){
-    if(val1 === val2){
-        return true
-    }else{
-        return false
+  if (typeof val1 != "object" && typeof val2 != "object") {
+    if (val1 === val2) {
+      return true;
+    } else {
+      return false;
     }
   }
 
   //check for deep equality if both are objects
-  if (typeof(val1) === "object" && typeof(val2) === "object"){
-    if(val1 === val2){
-        return true
-    }else{
-        return false
+  if (typeof val1 === "object" && typeof val2 === "object") {
+    //check if both or one is arrays
+    if (Array.isArray(val1) || Array.isArray(val2)) {
+      return false;
     }
+    //check if objects are same length
+    const keys1 = Object.keys(val1);
+    const keys2 = Object.keys(val2);
+
+    if (keys1 !== keys2) {
+      return false;
+    }
+
+    //check if there are match pairs for all the keys
+    if (!keys1.every((key) => keys2.includes(key))) {
+      return false;
+    }
+
+    //if all others check are good then do recursive comparison on keys in the object
+    for (let key in val1) {
+      let equal = deepEqual(val1[key], val2[key]);
+      if (!equal) {
+        return false;
+      }
+    }
+    return true;
   }
+}
 
 // Don't add or change anything below this comment.
 module.exports = deepEqual;
